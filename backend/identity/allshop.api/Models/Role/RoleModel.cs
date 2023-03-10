18c4c64a1e159d6118e1efb42de6cc72ea52c18e
all-developer <all.leonardo.lucero@gmail.com>
2023-03-09 21:25:54 -0300

    09032023

--- backend/identity/allshop.api/Models/Role/RoleModel.cs
+++ backend/identity/allshop.api/Models/Role/RoleModel.cs
@@ -2,5 +2,12 @@
 {
     public class RoleModel
     {
+    //"id": "string",
+    //"name": "string",
+    //"normalizedName": "string",
+    //"concurrencyStamp": "string",
+    //"active": true,
+    //"createdAt": "2023-03-07T17:46:57.770Z",
+    //"updatedAt": "2023-03-07T17:46:57.770Z"
     }
 }
